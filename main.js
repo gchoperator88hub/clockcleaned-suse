a846d894da22efaca6c3e64f7eb4a28d8f6ba1ef
gchoperator operatorov <gchoperator88@meta.ua>
2026-06-15 18:32:31 +0300

    add electron build files

---
+++ main.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+  });
+
+  // load local built files
+  win.loadFile(path.join(__dirname, "dist/index.html"));
+}
+
+app.whenReady().then(createWindow);
